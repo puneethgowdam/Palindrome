51ce8e3984a4c13e2b66767ef10133683a205638
HP <HP@DESKTOP-O09564I>
2022-08-22 18:56:29 +0530

    COMMIT

---
+++ Javaprojects/src/oops/Mainclass.java
@@ -0,0 +1,16 @@
+package oops;
+import java.util.*;
+import oopspakage.student;
+
+
+public class Mainclass {
+	public static void main(String[] args) {
+		
+		TEACHER teacher=new TEACHER();
+		
+		
+		
+		
+	}
+
+}
---
+++ Javaprojects/src/oopspakage/student.java
@@ -0,0 +1,5 @@
+package oopspakage;
+
+public class student {
+
+}
